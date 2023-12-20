76f2866646053b36dbb7fe3bdf444a835b2264ac
AndreyAndrianov jdplm-32 <darkc0lder@mail.ru>
2023-12-20 19:25:38 +0300

    4 сommit

--- src/main/java/com/example/cloud/service/FileStorageService.java
+++ src/main/java/com/example/cloud/service/FileStorageService.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.util.List;
 
 
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -27,6 +26,7 @@ public class FileStorageService {
 
     @Transactional
     public void putFile(MultipartFile multipartFile) {
+
         try {
             fileStorageRepository.save(File.builder().
                     fileName(multipartFile.getOriginalFilename()).
@@ -35,45 +35,62 @@ public class FileStorageService {
                     build());
             log.info("Файл загружен");
         } catch (IOException | RuntimeException e) {
+            log.info("Ошибка загрузки файла на ресурс");
             throw new RequestErrors(e.getMessage());
         }
 
     }
 
     @Transactional
-    public void deleteFile(String fileName) throws RequestErrors{
-            File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> new RequestErrors("Такого файла не существует"));
-            fileStorageRepository.delete(file);
-            log.info("Файл удален");
+    public void deleteFile(String fileName) throws RequestErrors {
+
+        File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> {
+            log.info("Ошибка в удалении файла");
+            return new RequestErrors("Такого файла не существует");
+        });
+        fileStorageRepository.delete(file);
+        log.info("Файл удален");
+
     }
 
     @Transactional
     public byte[] getFile(String fileName) throws RequestErrors {
-        File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> new RequestErrors("Такого файла не существует"));
-        log.info("Файл получен");
+
+        File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> {
+            log.info("Ошибка получения файла из ресурса");
+            return new RequestErrors("Такого файла не существует");
+        });
         return file.getFileSize();
 
     }
 
+
     @Transactional
-    public List <File> getAllFiles() throws RequestErrors {
+    public List<File> getAllFiles() throws RequestErrors {
+
         List<File> files = fileStorageRepository.findAll();
-            if (files.isEmpty()) {
-               throw new RequestErrors("Список пуст");
-            } else {
-                log.info("Cписок получен");
-                return List.copyOf(files);
-            }
+        if (files.isEmpty()) {
+            log.info("Ошибка в получение списка файлов из ресурса");
+            throw new RequestErrors("Список пуст");
+        } else {
+            log.info("Cписок получен");
+            return List.copyOf(files);
+        }
+
     }
 
     @Transactional
     public void editFileName(String fileName, String newFileName) throws RequestErrors {
-            File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> new RequestErrors("Такого файла не существует"));
-            file.setFileName(newFileName);
-            fileStorageRepository.save(file);
-            log.info("Название файла изменено");
-    }
 
+        File file = fileStorageRepository.findByFileName(fileName).orElseThrow(() -> {
+            log.info("Ошибка в изменении имени файла");
+            return new RequestErrors("Такого файла не существует");
+        });
+
+        file.setFileName(newFileName);
+        fileStorageRepository.save(file);
+        log.info("Название файла изменено");
+    }
 
 }
 
